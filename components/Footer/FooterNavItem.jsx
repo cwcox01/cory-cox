a18a5d3ba019b72ee71965156f4e27188e8a264c
Cory Cox <corycox@Corys-MBP.lan>
2023-04-23 23:16:59 -0400

    link fix

--- components/Footer/FooterNavItem.jsx
+++ components/Footer/FooterNavItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Link from "next/link";
 
 export default function FooterNavItem({ footerNavUrl, footerNavText }) {
-  return (
-    <li className="pe-3">
-      <a href={footerNavUrl}>{footerNavText}</a>
-    </li>
-  );
+	return (
+		<li className="pe-3">
+			<Link href={footerNavUrl}>{footerNavText}</Link>
+		</li>
+	);
 }
--- components/Heading/SocialmedianavItem.jsx
+++ components/Heading/SocialmedianavItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 export default function SocialmedianavItem({ socialBtnUrl, socialBtnText }) {
-  return (
-    <li className="ps-2 pt-1">
-      <button className="btn btn-secondary">
-        <a href={socialBtnUrl} target="_blank" rel="noreferrer">
-          <span>{socialBtnText}</span>
-        </a>
-      </button>
-    </li>
-  );
+	return (
+		<li className="ps-2 pt-1">
+			<button className="btn btn-secondary">
+				<Link href={socialBtnUrl} target="_blank" rel="noreferrer">
+					<span>{socialBtnText}</span>
+				</Link>
+			</button>
+		</li>
+	);
 }
