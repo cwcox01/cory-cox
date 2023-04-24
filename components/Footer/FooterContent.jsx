95f1dec87212c01f098f670b5f1504fba409a16c
Cory Cox <corycox@Corys-MBP.lan>
2023-04-23 23:27:28 -0400

    link fix

--- components/Footer/FooterContent.jsx
+++ components/Footer/FooterContent.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 export default function FooterContent() {
-  const today = new Date();
-  const year = today.getFullYear();
-  return (
-    <div className="footer-content d-flex flex-column align-items-center">
-      <p className="cory-footer-name mb-0">&copy; Cory Cox {year}</p>
-      <p className=" mb-0 footer-email">
-        <a className="footer-email" href="mailto: cory@corycox.com">
-          Email Me
-        </a>
-      </p>
-    </div>
-  );
+	const today = new Date();
+	const year = today.getFullYear();
+	return (
+		<div className="footer-content d-flex flex-column align-items-center">
+			<p className="cory-footer-name mb-0">&copy; Cory Cox {year}</p>
+			<p className=" mb-0 footer-email">
+				<Link className="footer-email" href="mailto: cory@corycox.com">
+					Email Me
+				</Link>
+			</p>
+		</div>
+	);
 }
