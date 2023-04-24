9935e479bea7004e7be70ad47f58945e2d2f3225
Cory Cox <corycox@Corys-MBP.lan>
2023-04-24 00:26:33 -0400

    link fix2

--- components/Footer/FooterItem.jsx
+++ components/Footer/FooterItem.jsx
@@ -1,20 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
-export default function FooterItem({
-  footerSocialClassName,
-  footerSocialUrl,
-  footerITagClassName,
-}) {
-  return (
-    <li className="pe-1">
-      <a
-        className="social-links"
-        href={footerSocialUrl}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className={footerITagClassName} aria-hidden="true"></i>
-      </a>
-    </li>
-  );
+export default function FooterItem({ footerSocialUrl, footerITagClassName }) {
+	return (
+		<li className="pe-1">
+			<Link
+				className="social-links"
+				href={footerSocialUrl}
+				target="_blank"
+				rel="noreferrer">
+				<i className={footerITagClassName} aria-hidden="true"></i>
+			</Link>
+		</li>
+	);
 }
